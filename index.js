5c0cfd459422e7d35690cbea7cddca4cfbe10aea
JoabeSimao <simaojoabecosta@gmail.com>
2022-04-28 09:53:22 -0300

    criar api-jogos

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+
+const app = express()
+
+app.use(bodyParser.json())
+const port = 6000
+
+app.get("/teste", (req, res) => res
+    .status(200)
+    .send({ mensagem: "bem vindo" })
+
+
+)
+
+app.listen(port, () => console.log(`listen on port:${port}`))
