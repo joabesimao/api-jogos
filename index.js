5e7bdf7a18b7f543ce8a708edf9de8abef91f988
JoabeSimao <simaojoabecosta@gmail.com>
2022-04-28 13:42:55 -0300

    mudar  mensagem

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ const bodyParser = require("body-parser")
 
 const app = express()
 
+
 app.use(bodyParser.json())
 const port = 5000
 
 app.get("/teste", (req, res) => res
     .status(200)
-    .send({ mensagem: "bem vindo" })
+    .send({ mensagem: "seja bem vindo" })
 
 
 )
+
 
 app.listen(port, () => console.log(`listen on port:${port}`))
